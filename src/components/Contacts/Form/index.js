e52cef79b35bc16996203e794a2790e8711bcd6c
sinan <aslansinan586@gmail.com>
2022-12-30 10:58:46 +0300

    create form

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import Contacts from "..";
+const initialFormValues = { fullname: "", phone_number: "" };
+function Form({ addContact, contacts }) {
+  const [form, setForm] = useState(initialFormValues);
 
-function Form() {
+  useEffect(() => {
+    setForm(initialFormValues); //form içi temizlendi
+  }, [contacts]);
+
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  
+  const onSubmit = (e) => {
+    e.preventDefault(); //form elementi kayıt yaptığında ynilenmesini durdurur.
+    if (form.fullname === "" || form.phone_number === "") {
+      //kontrol
+      return false;
+    }
+    addContact([...contacts, form]);
+  };
+
   return (
-    <div>Form</div>
-  )
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            name="fullname"
+            placeholder="Fullname"
+            value={form.fullname}
+            onChange={onChangeInput}
+          />
+        </div>
+        <div>
+          <input
+            name="phone_number"
+            placeholder="Phone Number"
+            value={form.phone_number}
+            onChange={onChangeInput}
+          />
+        </div>
+        <div>
+          <button onClick={onSubmit}>Add</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
